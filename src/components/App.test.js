010658c4385254916fb1de885b7a987274caa793
fredWills224 <fredwills4229@gmail.com>
2020-10-03 11:47:24 -0400

    app test clean

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,23 +1,14 @@
 import React from 'react';
-// import { shallow, mount, render } from 'enzyme';
-import {
-    createSerializer,
-    shallowToJson,
-    mountToJson,
-    renderToJson
-} from 'enzyme-to-json'; 
+import { shallow } from 'enzyme';
+import { createSerializer, shallowToJson } from 'enzyme-to-json'; 
 import App from './App';
 
 const app = shallow(<App/>);
 
+it('sets snapShotSerializer options to detailed snap-shots', ()=>{
+    expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+})
+
 it('renders correctly', ()=>{
-    // expect(app).toMatchSnapShot();
-    // expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
     expect(shallowToJson(app)).toMatchSnapshot();
-
 });
-
-// it('converts shallow wrappers', () => {
-//     const wrapper = shallow(<div>Hello</di>)
-//     expect(shallowToJson(wrapper)).toMatchSnapshot();
-// });
