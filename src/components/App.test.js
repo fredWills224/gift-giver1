39b1000ef32861c420a1c8d5b4f6ba7c2b2ac798
fredWills224 <fredwills4229@gmail.com>
2020-10-04 22:40:53 -0400

    upd person onChange

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,26 +3,38 @@ import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json'; 
 import App from './App';
 
-const app = shallow(<App/>);
+describe('App', ()=>{
+    const app = shallow(<App/>);
 
-it('renders correctly', ()=>{
-    expect(shallowToJson(app)).toMatchSnapshot();
+    it('renders correctly', ()=>{
+        expect(shallowToJson(app)).toMatchSnapshot();
+    });
+
+    it('initializes the `state` with an empty list of gifts', ()=>{
+        expect(app.state().gifts).toEqual([]);
+    });
+
+    describe('when clicking the `add-gift` button', ()=>{
+
+        beforeEach(()=>{
+            app.find('.btn-add').simulate('click');
+        });
+
+        afterEach(()=>{
+            app.setState({ gifts: [] });
+        });
+
+        it('adds a new gift to `state`', ()=>{
+            expect(app.state().gifts).toEqual([{ id: 1 }]);
+        });
+    
+        it('adds a new gift to the rendered list', ()=>{
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+
+    });
+
 });
-
-it('initializes the `state` with an empty list of gifts', ()=>{
-    expect(app.state().gifts).toEqual([]);
-});
-
-it('adds a new gift to `state` when clicking the `add gift` button', ()=>{
-    app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
-
-it('adds a new gift to the rendered list when clicking the `add gift` button', ()=>{
-    app.find('.btn-add').simulate('click');
-    expect(app.find('.gift-list').children().length).toEqual(2);
-});
-
 
 
 
